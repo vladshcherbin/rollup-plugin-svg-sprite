5e90d0e602147f8a1f28f242ce113006002611eb
Vlad Shcherbin <vlad.shcherbin@gmail.com>
2019-07-25 16:17:37 +0300

    Rewrite plugin, add svgo, change hooks, fix watch

--- src/index.js
+++ src/index.js
@@ -1,65 +1,77 @@
-import fs from 'fs'
 import path from 'path'
-import { createFilter } from 'rollup-pluginutils'
+import fs from 'fs-extra'
 import cheerio from 'cheerio'
-import pretty from 'pretty'
-import { minify } from 'html-minifier'
+import SVGO from 'svgo'
 
-function loadXml(xml) {
-  return cheerio.load(xml, { xmlMode: true })
-}
+function createSymbol(code, id) {
+  const markup = cheerio.load(code, { xmlMode: true })
+  const svgMarkup = markup('svg')
+  const symbolId = svgMarkup.find('title').text() || id
 
-function createSymbol(id, svg) {
-  const element = loadXml(svg)
-  const svgMarkup = element('svg')
-
-  element('svg').replaceWith('<symbol/>')
-  element('symbol')
-    .attr('id', id)
+  markup('svg').replaceWith('<symbol/>')
+  markup('symbol')
+    .attr('id', symbolId)
     .attr('viewBox', svgMarkup.attr('viewBox'))
     .append(svgMarkup.children())
 
-  return element.html()
+  return markup.xml('symbol')
 }
 
-function createSprite(symbols, prettify) {
-  const element = loadXml('<svg xmlns="http://www.w3.org/2000/svg"/>')
-
-  symbols.forEach((symbol) => {
-    element('svg').append(symbol)
-  })
-
-  return prettify
-    ? pretty(element.html())
-    : minify(element.html(), { collapseWhitespace: true })
+function createSprite(symbols) {
+  return `<svg xmlns="http://www.w3.org/2000/svg">${symbols.join('')}</svg>`
 }
 
 export default function svgSprite(options = {}) {
-  const filter = createFilter(options.include, options.exclude)
-  const prettify = options.prettify || false
-  const symbols = new Map()
+  const { minify = true, outputFolder, ...rest } = options
+
+  if (!outputFolder) {
+    throw new Error('"outputFolder" must be set')
+  }
+
+  const svgo = new SVGO({
+    js2svg: {
+      pretty: !minify,
+      indent: 2
+    },
+    plugins: [
+      { cleanupIDs: false },
+      ...Object.entries(rest).map(([plugin, params]) => ({ [plugin]: params }))
+    ]
+  })
+  const loadedSvgs = new Set()
+  const convertedSvgs = new Map()
 
   return {
     name: 'svg-sprite',
-    transform: (code, id) => {
-      if (!filter('id') || path.extname(id) !== '.svg') {
+    load(id) {
+      if (id.endsWith('.svg')) {
+        loadedSvgs.add(id)
+      }
+
+      return null
+    },
+    transform(code, id) {
+      if (!id.endsWith('.svg')) {
         return null
       }
 
-      const svgId = path.parse(id).name
+      const filename = path.basename(id, '.svg')
 
-      symbols.set(svgId, createSymbol(svgId, code))
+      convertedSvgs.set(id, createSymbol(code, filename))
 
       return {
-        code: '',
-        map: { mappings: '' }
+        code: ''
       }
     },
-    generateBundle: (details) => {
-      if (symbols.size) {
-        const outputFolderPath = options.outputFolder || path.dirname(details.file)
+    async writeBundle() {
+      if (loadedSvgs.size) {
+        const symbols = [...loadedSvgs.values()].map(id => convertedSvgs.get(id))
+        const { data } = await svgo.optimize(createSprite(symbols))
 
-        fs.writeFileSync(`${outputFolderPath}/sprites.svg`, createSprite(symbols, prettify))
+        await fs.ensureDir(outputFolder)
+        await fs.writeFile(`${outputFolder}/sprites.svg`, data)
+
+        loadedSvgs.clear()
       }
     }
   }
